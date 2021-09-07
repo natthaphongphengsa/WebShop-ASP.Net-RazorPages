960e972a7ae86aa958323c533836effc7429c656
Natthaphong Phengsa <natthaphong.phengsa.syne20lin@tucsweden.se>
2021-09-07 15:55:19 +0200

    Fix Only string allowed when adding Category

--- WebbShop/Areas/Identity/Pages/Admin/Management/CreateCategory.cshtml.cs
+++ WebbShop/Areas/Identity/Pages/Admin/Management/CreateCategory.cshtml.cs
@@ -19,6 +19,7 @@ namespace WebbShop.Pages.Admin.Management
         [BindProperty]
         [Required(ErrorMessage = "Category name is required!")]
         [DataType(DataType.Text)]
+        [RegularExpression("^[a-zA-Z]*$",ErrorMessage = "Only text allowed!")]
         public string Name { get; set; }
 
         private readonly INotyfService notyf;
